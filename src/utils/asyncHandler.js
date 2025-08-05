86791800b919ac824ee73de1861697722ed0f111
Puneet-TH <22BCS13944@cuchd.in>
2025-08-05 20:10:58 +0530

    added why we need async handler in async handler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,3 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next))
-        .catch((err) => next(err))
-    }
-}
-
-export {asyncHandler}
-
 // const asyncHandler = (fun) => async(req, res, next) => {
 //      try {
 //         await fn(req, res, next)
@@ -17,3 +8,38 @@ export {asyncHandler}
 //         })
 //      }
 // }
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+/*
+you dont need to intall any npm package:
+
+instead write this :
+
+const asyncHandler = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+Without this wrapper, you'd need to write try/catch in every async route:
+
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await somethingAsync();
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+With asyncHandler, you can simplify it:
+
+router.get("/", asyncHandler(async (req, res) => {
+  const data = await somethingAsync();
+  res.json(data);
+}));
+so instead of writing try/catch block inside your endpoint handlers use the asyncHandler.
+
+*/
