177f269c3f8ca1a580a0392f9af638728502ab96
Puneet-TH <22BCS13944@cuchd.in>
2025-08-05 19:27:04 +0530

    added user router's

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAcessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword,
+     getCurrentUser, 
+     getUserChannelProfile, 
+     getWatchHitory, 
+     loginUser, 
+     logoutUser, 
+     refreshAcessToken,
+      registerUser, 
+      updateAccountDetails, 
+      updateUserAvatar, 
+      updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
+import { verify } from "jsonwebtoken";
 
 const router = Router()
 
@@ -24,6 +36,12 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post( verifyJWT, logoutUser)
 router.route("/refresh-Token").post(refreshAcessToken)
-
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage),
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHitory)
 
 export default router
