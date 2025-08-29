1e438210860412b9b44ecb877da3912996dbdc0e
Puneet-TH <22BCS13944@cuchd.in>
2025-08-29 20:28:19 +0530

    added logo on top

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -5,7 +5,6 @@ import fs from "fs"
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET,
-        secure: true 
     });
 
 const uploadOnCloudinary = async (localFilePath) => {
